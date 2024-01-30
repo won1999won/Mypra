93ed6c248158e4797c6e3e9d61f8dfc15f0e5dce
won1999won <won1999won6@gmail.com>
2024-01-30 13:20:56 +0900

    aa

--- chap04-control-flow-lecture-source/src/main/java/com/oghraffers/section01/condional/D_swith.java
+++ chap04-control-flow-lecture-source/src/main/java/com/oghraffers/section01/condional/D_swith.java
@@ -27,14 +27,57 @@ public class D_swith {
         int result= 0;
 
         switch (op){
-            case '+':result=fName+sName;
-            case '-':result=fName+sName;
-            case '*':result=fName*sName;
-            case '/':result=fName/sName;
-            case '%':result=fName%sName;
+            case '+':result=fName+sName;break;
+            case '-':result=fName+sName;break;
+            case '*':result=fName*sName;break;
+            case '/':result=fName/sName;break;
+            case '%':result=fName%sName;break;
             default:
                 System.out.println("error");
         }
+        System.out.println(fName+" "+op+" "+sName+" "+result);
+
+    }
+    public void switchvendingmachine(){
+        /*스위치-케이스문 이용실습
+        * 1.스캐너를 이용해 음료 이름(스트링)과 갯수(인트)입력
+        * 2.스위치 케이스문을 사용해 전체 가격을 구한다
+        * 3.[음료이름 을 개수 개 선택하셧습니다 가격 을 투입하여 주십시오]출력되게한다
+        *
+        * */
+        System.out.println("====자판기====");
+        /*콜라 1700/환타4500/사이다2300/커피8900*/
+        System.out.println("코카콜라 환타 사이다 커피");
+        System.out.println("==============");
+
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("name");
+        String name= sc.nextLine();
+        System.out.println("number");
+        int number=sc.nextInt();
+
+        int result= 0;
+
+
+        switch (name){
+            case "코카콜라":result=1700*number;break;
+            case "환타":result=4500*number;break;
+            case "사이다":result=2300*number;break;
+            case "커피":result=8900*number;break;
+            default:
+                System.out.println("error");
+
+        }
+        System.out.println(name+"를"+number+"개 선택하셧습니다"+result+"을 넣어주세요");
+
+
+
+
+
+
+
+
 
     }
 }
