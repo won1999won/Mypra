61d3ccd372dd252311245c9168a87b6780515493
won1999won <won1999won6@gmail.com>
2024-01-30 12:34:03 +0900

    aa

--- chap04-control-flow-lecture-source/src/main/java/com/oghraffers/section01/condional/Application1.java
+++ chap04-control-flow-lecture-source/src/main/java/com/oghraffers/section01/condional/Application1.java
@@ -1,4 +1,16 @@
 package com.oghraffers.section01.condional;
 
 public class Application1 {
-}
+    public static void main(String[] args) {
+        C_ifelse c=new C_ifelse();
+        c.nestedifelseifstatement();
+
+
+    }
+
+
+
+
+
+    }
+
---
+++ chap04-control-flow-lecture-source/src/main/java/com/oghraffers/section01/condional/D_swith.java
@@ -0,0 +1,15 @@
+package com.oghraffers.section01.condional;
+
+public class D_swith {
+
+    public void cimpleswitchstatemment(){
+
+        /*
+        * 스위치문 표현식
+        * 스위치(비교할변수){
+        *
+        * }
+        * */
+
+    }
+}
