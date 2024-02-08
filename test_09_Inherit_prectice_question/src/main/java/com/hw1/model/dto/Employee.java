b4a9b8444f68a58aadcbce05566327b0d1375f5d
won1999won <won1999won6@gmail.com>
2024-02-08 18:22:38 +0900

    asdasdasdasdsadsadsad

---
+++ test_09_Inherit_prectice_question/src/main/java/com/hw1/model/dto/Employee.java
@@ -0,0 +1,50 @@
+package com.hw1.model.dto;
+
+public class Employee extends Person{
+
+    private int salary;
+    private String dept;
+
+
+    public Employee(){}
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "salary=" + salary +
+                ", dept='" + dept + '\'' +
+                '}';
+    }
+
+    public Employee(int salary, String dept) {
+        this.salary = salary;
+        this.dept = dept;
+    }
+
+    public Employee(String name, int age, double height, double weight, int salary, String dept) {
+        super(age, height, weight);
+        setName(name);
+        this.salary = salary;
+        this.dept = dept;
+    }
+    public String information(){
+         return "salary:"+ salary+"dept"+dept;
+
+    }
+}
---
+++ test_09_Inherit_prectice_question/src/main/java/com/hw1/run/Run.java
@@ -0,0 +1,65 @@
+package com.hw1.run;
+
+import com.hw1.model.dto.Employee;
+import com.hw1.model.dto.Person;
+import com.hw1.model.dto.Student;
+
+import java.util.Scanner;
+
+public class Run {
+    public static void main(String[] args) {
+
+
+        Person[] students={
+                new Student("홍길이",20,170.2,70.0,1,"정보시스템공학과"),
+                new Student("말똥이",21,187.2,80.0,2,"경영학과"),
+                new Student("개순이",23,152.2,45.0,4,"정보통신공학과")
+        };
+        for (Person i:students){
+            System.out.println(i.getinfo());
+        }
+
+
+        Employee[] employee =new Employee[10];
+        Scanner scanner=new Scanner(System.in);
+        int employeeCt=0;
+        String in=scanner.next();
+        int countem=0;
+
+        do {
+            System.out.print("이름:");
+            String name=scanner.nextLine();
+            System.out.println("나이");
+            int age= scanner.nextInt();
+            System.out.println("신장");
+            double height=scanner.nextDouble();
+            System.out.println("몸무게");
+            double weight= scanner.nextDouble();
+            System.out.println("급여");
+            int saraly=scanner.nextInt();
+            System.out.println("부서");
+            String dept=scanner.nextLine();
+
+            employee[employeeCt]=new Employee(name,age,height,weight,saraly,dept);
+
+            countem++;
+            scanner.nextLine();
+            System.out.print("계속입력? y/n");
+            in=scanner.next();
+
+
+        }while(!in.equals("n"));
+            System.out.println("종료");
+
+
+        for (int i=0;i<employee.length;i++){
+            System.out.println(employee[i]);
+            System.out.println(countem);
+        }
+    }
+
+
+
+
+
+}
